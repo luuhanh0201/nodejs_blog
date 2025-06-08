93690728f8a292108ce45a474e5137af4a0299ef
Luu Hanh <luuhanh0201@gmail.com>
2025-06-08 23:00:23 +0700

    day 2.1: MVC and Controllers & Routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const express = require("express");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const handlebars = require("express-handlebars");
-const app = express();
 const post = 3000;
-
+const app = express();
+const route = require("./routes/index.js");
 app.use(express.static(path.join(__dirname, "public")));
 
+
 // Morgan is a logging library for Node.js that provides HTTP request logging
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 app.engine("handlebars", handlebars());
 
 // Handlebars setup
@@ -22,13 +23,8 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
 // rote
+route(app);
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
-app.get("/news", (req, res) => {
-    res.render("news");
-});
 app.listen(post, () => {
     console.log(`Server is running on http://localhost:${post}`);
 });
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const newsRouter = require("./news");
+function route(app) {
+    app.use("/news", newsRouter);
+    
+
+
+
+    app.get("/", (req, res) => {
+        res.render("home");
+    });
+    app.get("/search", (req, res) => {
+        res.render("search");
+    });
+}
+module.exports = route;
